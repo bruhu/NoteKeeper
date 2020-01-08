d561a240bee31b6998df1681bf3b7b8a210f521b
Anna Brugera <anna.brugera@cgi.com>
2020-01-08 15:50:58 +0100

    Created Intent in NoteList Activity

--- src/main/java/com/bruhu/notekeeper/NoteListActivity.kt
+++ src/main/java/com/bruhu/notekeeper/NoteListActivity.kt
@@ -1,5 +1,6 @@
 package com.bruhu.notekeeper
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -13,8 +14,12 @@ class NoteListActivity : AppCompatActivity() {
         setContentView(R.layout.activity_note_list)
         setSupportActionBar(toolbar)
 
+        // code for launching our Activity to edit Notes will go here
         fab.setOnClickListener { view ->
-            
+            // create an intent
+            var activityIntent = Intent(this, MainActivity::class)
+            // start the activity method
+            startActivity()
         }
     }
 
